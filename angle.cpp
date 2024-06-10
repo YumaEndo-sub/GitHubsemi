8c4bc00174ff77ff0feae3a108baef346b922e3f
Yuma Endo <146794326+YumaEndo-itt@users.noreply.github.com>
2024-06-10 11:30:50 +0900

    Add files via upload

---
+++ angle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	double v1={1,2,3};
+	double v2={4,5,6};
+
+//ベクトルv1,v2の内積を計算する
+
+//ベクトルv1,v2の大きさをそれぞれ計算する
+
+//ベクトルv1,v2のなす角を計算する
+
+//計算結果を表示する
+
+
